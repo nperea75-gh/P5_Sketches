3e4f302723d0d7c286d02d335103133503da1599
ney roger <52115064+nperea75-gh@users.noreply.github.com>
2020-03-29 19:27:05 -0500

    cambio en el index

--- p5/spiral_ellipse.js
+++ p5/spiral_ellipse.js
@@ -2,21 +2,23 @@ var r = 0.05;
 
 function setup() {
   createCanvas(400, 400);
-
+  frameRate(20);
   background(220);
 }
 
 function draw() {
-
-  noFill();
-  frameRate(20);
-  translate(width / 2, height / 2);
-  strokeWeight(2);
+if(r>200){
+    background(220);
+    r=1;
+}
+ else{ 
+      
+     translate(width / 2, height / 2);  
   fill(random(1,255),random(1,255),random(1,255));
   r++
   rectMode(CENTER);
   rotate(r);
-  ellipse(r,  r , height / 8,height / 8 )
+  ellipse(r,  r , height / 8,height / 8 )}
 
 
 }
--- p5/spiral_rect.js
+++ p5/spiral_rect.js
@@ -2,21 +2,24 @@ var r = 0.05;
 
 function setup() {
   createCanvas(400, 400);
-
+ frameRate(20);
   background(220);
 }
 
 function draw() {
-
-  noFill();
-  frameRate(20);
-  translate(width / 2, height / 2);
-  strokeWeight(2);
+if (r>300){
+     background(220)
+    r=1
+}
+else{
+     
+    translate(width / 2, height / 2);
+  
   fill(random(1,255),random(1,255),random(1,255));
   r++
   rectMode(CENTER);
   rotate(r);
   rect(r,  width / 8, height / 8, r)
-
+}
 
 }
